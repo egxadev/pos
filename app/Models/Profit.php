6c0a0290fae08344ce13ca07f8bcb1bbf9d8e367
Ega Alifya Firnando <ega.alfy@gmail.com>
2022-08-24 18:46:18 +0700

    create accessors

--- app/Models/Profit.php
+++ app/Models/Profit.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,5 +28,17 @@ class Profit extends Model
     public function transaction()
     {
         return $this->belongsTo(Transaction::class);
+    }
+
+    /**
+     * createdAt
+     *
+     * @return Attribute
+     */
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d-M-Y H:i:s')
+        );
     }
 }
